618cb1e69b0325436ecd6bb288da9d698f4f3c2f
pwarren6 <warren46892@gmail.com>
2016-02-19 18:43:01 -0500

    Created Loader class

--- src/Driver.java
+++ src/Driver.java
@@ -2,4 +2,11 @@
  * Created by qazimusab on 2/19/16.
  */
 public class Driver {
+    //Loader load;
+    //loop
+    ///Scheduler schedule;
+    ///Dispatcher dispatch;
+    ///CPU cpu;
+    ///WaitInterrupt();
+    //end loop
 }
---
+++ src/Loader.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Patrick on 2/19/2016.
+ */
+public class Loader {
+}
