57e9f9c133c983a849141f63f0c6331ef8c07f1b
pwarren6 <warren46892@gmail.com>
2016-02-28 17:18:07 -0500

    Update (2/28) - Added variables to Process.java

--- src/Process.java
+++ src/Process.java
@@ -1,5 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Created by qazimusab on 2/19/16.
  */
 public class Process {
+    List<String> job_opcode;
+    List<String> data_opcode;
+    public ArrayList total_page;
+    public int JobNumber;
+    public int NumberofInstructions;
+    public int Priority;
+    public int inputBufferSize;
+    public int outputBufferSize;
+    public int tempBufferSize;
+    public int DataAddress;
+
+    public Process(){
+        JobNumber = 0;
+        NumberofInstructions = 0;
+        Priority = 0;
+        inputBufferSize = 0;
+        outputBufferSize = 0;
+        tempBufferSize = 0;
+        DataAddress = 0;
+    }
 }
