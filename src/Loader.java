abf0a00852250fd05c2392849f8cc495f9862b3e
pwarren6 <warren46892@gmail.com>
2016-03-05 18:41:10 -0500

    Update (3/05)

--- src/Loader.java
+++ src/Loader.java
@@ -2,5 +2,19 @@
  * Created by Patrick on 2/19/2016.
  */
 public class Loader {
+    public static int HextoDecimal(String Hex){
+        return Integer.parseInt(Hex, 16);
+    }
+    public static String HextoBinary(String Hex){
+        Integer input = Integer.parseInt(Hex, 16);
+        return Integer.toBinaryString(input);
+    }
+    public static int BinarytoDecimal(String Binary){
+        return Integer.parseInt(Binary, 2);
+    }
+    public static String DecimaltoBinary(String Decimal){
+        Integer input = Integer.parseInt(Decimal);
+        return Integer.toBinaryString(input);
 
+    }
 }
