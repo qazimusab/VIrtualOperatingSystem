71a69200863a7a518479c2b55a4b400b286b46ee
pwarren6 <warren46892@gmail.com>
2016-03-15 19:24:19 -0400

    Update (3/15)

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -2,19 +2,19 @@
  * Created by qazimusab on 2/19/16.
  */
 public class Scheduler {
-    private static int pc = 0;
-    public static Boolean LongTermScheduler(Disk d, RAM r, PCB p){
-        if (pc < d.TotalProcess() && r.remaining_size >= d.GetProcess(pc).Opcode_Size()){
+    private static int programCounter = 0;
+    public static Boolean LongTermScheduler(Disk d, RAM r, PCB pcb){
+        if (programCounter < d.TotalProcess() && r.remaining_size >= d.GetProcess(programCounter).Opcode_Size()){
             while(r.changing);
-            while(p.changing);
+            while(pcb.changing);
             //Add to the RAM
-            r.Add_Process(d.GetProcess(pc));
-            MMU q = new MMU(d.GetProcess(pc), r.Total_Process() - 1);
+            r.Add_Process(d.GetProcess(programCounter));
+            MMU mmu = new MMU(d.GetProcess(programCounter), r.Total_Process() - 1);
             //Then add the queue to PCB
-            p.AddQueue(q);
-            System.out.println("Added job to RAM: " + d.GetProcess(pc++).JobNumber);
+            pcb.AddQueue(mmu);
+            System.out.println("Added job to RAM: " + d.GetProcess(programCounter++).JobNumber);
             r.changing = false;
-            p.changing = false;
+            pcb.changing = false;
             return false;
         }
         else
